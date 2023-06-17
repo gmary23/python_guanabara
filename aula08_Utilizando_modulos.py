e86fed084af85c80a701b6403421b59423b6de91
gmary23 <geila.estudos@gmail.com>
2023-06-17 15:49:02 -0300

    Utilizando e importando módulos

---
+++ aula08_Utilizando_modulos.py
@@ -0,0 +1,12 @@
+# print('UMA FORMA DE FAZER RAIZ QUADRADA')
+# import math
+# num = int(input('Digite um número: '))
+# raiz = math.sqrt(num)
+# print('A raiz de {} é igual a {}'.format(num, raiz))
+
+print()
+print('OUTRA FORMA DE FAZER RAIZ QUADRADA')
+from math import sqrt
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print('A raiz de {} é igual a {}'.format(num, raiz))
