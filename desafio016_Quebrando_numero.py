53e4390b13ea7601178dc85c63440fd21a448f4a
gmary23 <geila.estudos@gmail.com>
2023-06-17 15:50:09 -0300

    Transformando números para números inteiros de duas formas

---
+++ desafio016_Quebrando_numero.py
@@ -0,0 +1,10 @@
+"""
+Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+"""
+# from math import trunc
+# numero = float(input('Digite um número: '))
+# print('O valor informado é {} e sua porção inteira é {}'.format(numero, trunc(numero)))
+
+print('OUTRA FORMA DE RESOLVER O MESMO EXERCÍCIO')
+numero = float(input('Digite um número: '))
+print('O valor informado é {} e sua porção inteira é {}'. format (numero, int(numero)))
